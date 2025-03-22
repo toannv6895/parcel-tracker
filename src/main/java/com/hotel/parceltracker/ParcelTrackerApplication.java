b8e8c137d5b30cd079e317d3a4cdfa80477af6db
Toan Nguyen <tnguyen@us.maxnerva.com>
2025-03-22 17:38:17 +0700

    init project

---
+++ src/main/java/com/hotel/parceltracker/ParcelTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.parceltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotel/parceltracker/ParcelTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.parceltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
